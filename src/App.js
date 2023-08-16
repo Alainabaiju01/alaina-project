2b201f4a361e0ab46e30befcdd3dfc6406a62d79
alainabaiju01 <alaina.csa2125@saintgits.org>
2023-08-16 17:29:45 +0530

    updated the app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to my first project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jolly-caramel-2cf89b.netlify.app"
           target="_blank"
           rel="noopener noreferrer"
         >
